86d44e6428a687c8adde6b6988e631bbb3d528de
Dan Bikle <dan.bikle@gmail.com>
2015-03-20 23:22:37 +0000

    hi

--- rbm10.py
+++ rbm10.py
@@ -10,5 +10,10 @@ import numpy  as np
 import sys
 
 df1 = pd.read_csv('ftrGSPC2.csv')
-pdb.set_trace()
+
 df1.head()
+
+import sklearn.neural_network
+
+pdb.set_trace()
+rbm1 = sklearn.neural_network.BernoulliRBM
