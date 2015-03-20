b026e430f29971b73d4cb8ffa139dc4ee018fbc5
Dan Bikle <dan.bikle@gmail.com>
2015-03-20 23:30:59 +0000

    hi

--- rbm10.py
+++ rbm10.py
@@ -13,7 +13,25 @@ df1 = pd.read_csv('ftrGSPC2.csv')
 
 df1.head()
 
+# I should declare some integers to help me navigate the Arrays.
+
+cdate_i   = 0
+cp_i      = 1
+pctlead_i = 2
+x1_i      = 3
+x2_i      = 4
+x3_i      = 5
+x4_i      = 6
+
+# sklearn likes numpy arrays. I should convert my dataframe to array:
+wide_a = np.array(df1)
+
+# I should separate columns into x, y columns:
+x_a    = wide_a[:,x1_i:x4_i]
+y_a    = wide_a[:,pctlead_i]
+
 import sklearn.neural_network
 
 pdb.set_trace()
-rbm1 = sklearn.neural_network.BernoulliRBM
+rbm1 = sklearn.neural_network.BernoulliRBM(n_components=2)
+rbm1.fit(x_a, y_a)
